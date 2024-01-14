835ac2143673a9af84c748030513f7518f4c5900
nmur <null@gmail.com>
2024-01-15 00:07:42 +1100

    modified hello world text to output 240p

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,13 @@
 
 #include "picovga.h"
 
-// EGA (512x400) frame buffer for display
-ALIGNED u8 Framebuffer[512*400];
+ALIGNED u8 Framebuffer[320*240];
 
 int main()
 {
 	// Initialize video mode
-	Video(DEV_VGA, RES_EGA, FORM_8BIT, Framebuffer);
+	Video(DEV_NTSC, RES_QVGA, FORM_8BIT, Framebuffer);
 
 	// Draw text
-	DrawText(&Canvas, "Hello World!", (512-12*8*4)/2, (400-8*4)/2,
-		COL_WHITE, FontBoldB8x16, 16, 4, 4);
+	DrawText(&Canvas, "Hello World!", (320-12*8*4)/2, (240-8*4)/2, COL_WHITE, FontBoldB8x16, 16, 4, 4);
 }
