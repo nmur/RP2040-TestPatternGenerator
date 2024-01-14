b583aca9e8015b6bd9407dced9c23384e3842ea5
nmur <null@gmail.com>
2024-01-15 01:55:00 +1100

    added monoscope file and attempted display it on screen

---
+++ img/monoscope.h
@@ -0,0 +1,3 @@
+#include "picovga.h"
+
+extern ALIGNED const u8 Monoscope[24160];
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,25 @@
-
 #include "picovga.h"
+#include "img/monoscope.h"
+
+#define WIDTH	320		// screen width
+#define HEIGHT	240		// screen height
 
 ALIGNED u8 Framebuffer[320*240];
+
+sCanvas PatternCanvas;
 
 int main()
 {
 	// Initialize video mode
 	Video(DEV_NTSC, RES_QVGA, FORM_8BIT, Framebuffer);
 
-	// Draw text
-	DrawText(&Canvas, "Hello World!", (320-12*8*4)/2, (240-8*4)/2, COL_WHITE, FontBoldB8x16, 16, 4, 4);
+	// Initialise pattern canvas
+	PatternCanvas.img = (u8*)Monoscope;
+	PatternCanvas.w = WIDTH;
+	PatternCanvas.h = HEIGHT;
+	PatternCanvas.wb = WIDTH;
+	PatternCanvas.format = CANVAS_8;
+
+	// Draw pattern
+	DrawImg(&Canvas, &PatternCanvas, 0, 0, 0, 0, WIDTH, HEIGHT);
 }
