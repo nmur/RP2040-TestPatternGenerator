ed1eb4374f3a49c503dd22d742b2a8dc3090a3cf
nmur <null@gmail.com>
2024-01-28 22:03:34 +1100

    added basic button handling (just displays text on screen)

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,22 @@
 #include "picovga.h"
 #include "img/monoscope.h"
+#include "lib/RP2040-Button/button.h"
 
 #define WIDTH	320		// screen width
 #define HEIGHT	240		// screen height
 
+#define BUTTON_CYCLE_PATTERN 2
+
 ALIGNED u8 Framebuffer[320*240];
 
 sCanvas PatternCanvas;
+
+void button_onchange(button_t *button_p){
+    button_t *button = (button_t*)button_p;
+    if(button->state) return;
+
+    DrawText(&Canvas, "Button Press!", (512-12*8*4)/2, (400-8*4)/2, COL_WHITE, FontBoldB8x16, 16, 4, 4);
+}
 
 int main()
 {
@@ -22,4 +32,12 @@ int main()
 
 	// Draw pattern
 	DrawImg(&Canvas, &PatternCanvas, 0, 0, 0, 0, WIDTH, HEIGHT);
+
+	// Button handling
+    create_button(BUTTON_CYCLE_PATTERN, button_onchange);
+
+	while (true)
+	{
+        sleep_ms(100);
+	}
 }
