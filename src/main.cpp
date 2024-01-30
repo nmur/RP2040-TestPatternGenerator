9c8a73d92798cb8f5b09e31c18289cc959e82515
nmur <null@gmail.com>
2024-01-30 19:03:25 +1100

    added long press handling to reboot into flash mode

--- src/main.cpp
+++ src/main.cpp
@@ -4,11 +4,18 @@
 #include "img/circles.h"
 #include "img/grid.h"
 #include "lib/RP2040-Button/button.h"
+#include "pico/bootrom.h"
+
+#include <chrono>
 
 #define WIDTH	320		// screen width
 #define HEIGHT	240		// screen height
 
-#define BUTTON_CYCLE_PATTERN 2
+#define GPIO_CYCLE_PATTERN 2
+
+using sys_clock_t = std::chrono::high_resolution_clock;
+using time_point_t = std::chrono::time_point<sys_clock_t>;
+time_point_t buttonPressTime;
 
 const u8* PatternArray[] = {
 	Colour_Bars,
@@ -25,12 +32,27 @@ sCanvas PatternCanvas;
 
 void button_onchange(button_t *button_p){
     button_t *button = (button_t*)button_p;
-    if(button->state) return;
 
-	PatternIndex = (PatternIndex + 1) % PatternArraySize;
+    if (button->state) {
+        // Button release
+        // Calculate the duration of the button press
+        auto duration = std::chrono::duration_cast<std::chrono::seconds>(sys_clock_t::now() - buttonPressTime).count();
 
-	PatternCanvas.img = (u8*)PatternArray[PatternIndex];
-	DrawImg(&Canvas, &PatternCanvas, 0, 0, 0, 0, WIDTH, HEIGHT);
+        if (duration >= 3) {
+            // Reboot into BOOLSEL for flashing
+            reset_usb_boot(0,0);
+        }
+
+        return; // Ignore button release.
+    }
+    else {
+        // Button press
+        buttonPressTime = sys_clock_t::now();
+    }
+
+    PatternIndex = (PatternIndex + 1) % PatternArraySize;
+    PatternCanvas.img = (u8*)PatternArray[PatternIndex];
+    DrawImg(&Canvas, &PatternCanvas, 0, 0, 0, 0, WIDTH, HEIGHT);
 }
 
 int main()
@@ -49,7 +71,7 @@ int main()
 	DrawImg(&Canvas, &PatternCanvas, 0, 0, 0, 0, WIDTH, HEIGHT);
 
 	// Button handling
-    create_button(BUTTON_CYCLE_PATTERN, button_onchange);
+	create_button(GPIO_CYCLE_PATTERN, button_onchange);
 
 	while (true)
 	{
